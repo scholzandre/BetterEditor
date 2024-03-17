ecd24198c50bddb1cdffeea28d64d3244c9a039a
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-17 12:10:50 +0100

    #add properties, constructor and methods

--- BetterEditor/Models/DataManager.cs
+++ BetterEditor/Models/DataManager.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using BetterEditor.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BetterEditor.Models {
     internal class DataManager {
+        public Settings Settings { get; set; }
+        public List<ViewMode> ViewModes { get; set; }
+        public List<Tab> Tabs { get; set; }
+        private string _filePath = "";
+
+        public DataManager(Settings settings, List<ViewMode> viewModes, List<Tab> tabs) {
+            Settings = settings;
+            ViewModes = viewModes;
+            Tabs = tabs;
+        }
+        public DataManager() { }
+
+        public static DataManager GetData() {
+            throw new NotImplementedException();
+        }
+
+        public static bool WriteData() {
+            throw new NotImplementedException();
+        }
+
+        public static Settings GetSettings() {
+            throw new NotImplementedException();
+        }
+
+        public static List<ViewMode> GetViewModes() {
+            throw new NotImplementedException();
+        }
+
+        public static List<Tab> GetTabs() {
+            throw new NotImplementedException();
+        }
+
+        public static string GetFilePath() {
+            throw new NotImplementedException();
+        }
     }
 }
