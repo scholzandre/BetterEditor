cdfe8c45c4123b06c2fb65996d6581daccf04bf4
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-17 14:24:14 +0100

    #add OnPropertyChanged and ShowErrorMessage method

--- BetterEditor/ViewModels/BaseViewModel.cs
+++ BetterEditor/ViewModels/BaseViewModel.cs
@@ -1,10 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
 
 namespace BetterEditor.ViewModels {
-    internal class BaseViewModel {
+    internal class BaseViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// shows error in a MessageBox and writes it in console
+        /// </summary>
+        /// <param name="exception"></param>
+        public static void ShowErrorMessage(Exception exception) {
+            string infoText = "An error occured";
+            Debug.WriteLine($"{infoText}\n{exception.Message}");    
+            MessageBox.Show(exception.Message, infoText);
+        }
     }
 }
