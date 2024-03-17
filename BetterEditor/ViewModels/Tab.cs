8b40964f0e89515663a45648e283acc31f17543e
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-17 11:18:20 +0100

    #add Tab class with properties + constructor

---
+++ BetterEditor/ViewModels/Tab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterEditor.ViewModels {
+    internal class Tab {
+        /// <summary>
+        /// File Path. Will be a random number if no linked file.
+        /// </summary>
+        public string FilePath { get; set; }
+
+        /// <summary>
+        /// Tab Content
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// MD = Modification Date
+        /// </summary>
+        public DateOnly MD { get; set; }
+
+        public Tab(string filePath, string content, DateOnly mD) { 
+            FilePath = filePath;
+            Content = content;
+            MD = mD;
+        }
+    }
+}
