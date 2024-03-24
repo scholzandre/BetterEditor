ceb3150b6cb623f509e7abeaf2cae6efb88250ba
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-24 20:32:19 +0100

    #add properties and constructor

--- BetterEditor/ViewModels/TextEditorViewModel.cs
+++ BetterEditor/ViewModels/TextEditorViewModel.cs
@@ -5,6 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BetterEditor.ViewModels {
-    internal class TextEditorViewModel {
+    internal class TextEditorViewModel : BaseViewModel {
+        private List<Tab> _tabs;
+        public List<Tab> Tabs {
+            get => _tabs;
+            set {
+                _tabs = value; 
+                OnPropertyChanged(nameof(Tabs));
+            }
+        }
+        public Settings Settings { get; set; }
+        public TextEditorViewModel(List<Tab> tabs, Settings settings) {
+            Tabs = tabs;
+            Settings = settings;
+        }
     }
 }
