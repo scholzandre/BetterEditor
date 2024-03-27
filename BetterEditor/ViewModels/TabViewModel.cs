a2eec5d8baf3eca9d4d2c4dc41939a87c4ca1075
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-27 20:51:12 +0100

    #add TabViewModel

---
+++ BetterEditor/ViewModels/TabViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterEditor.ViewModels {
+    class TabViewModel : Tab {
+        public string TabName { get; set; }
+        public bool IsActive { get; set; }
+        public TabViewModel(string filePath = "", string content = "", DateOnly mD = default(DateOnly), string tabName = "", bool isActive = true) : base(filePath, content, mD) { 
+            TabName = tabName;
+            IsActive = isActive;
+        }
+    }
+}
