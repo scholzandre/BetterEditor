28a73a870af826732309f4d59db452e5498ddd55
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-03-28 19:12:53 +0100

    #add property and equals method

--- BetterEditor/ViewModels/TabViewModel.cs
+++ BetterEditor/ViewModels/TabViewModel.cs
@@ -8,9 +8,19 @@ namespace BetterEditor.ViewModels {
     class TabViewModel : Tab {
         public string TabName { get; set; }
         public bool IsActive { get; set; }
-        public TabViewModel(string filePath = "", string content = "", DateOnly mD = default(DateOnly), string tabName = "", bool isActive = true) : base(filePath, content, mD) { 
+        public int Index { get; set; }
+        public TabViewModel(string filePath = "", string content = "", DateOnly mD = default(DateOnly), string tabName = "", bool isActive = true, int index = 0) : base(filePath, content, mD) { 
             TabName = tabName;
             IsActive = isActive;
+            Index = index;
+        }
+        public override bool Equals(object obj) {
+            if (obj == null || GetType() != obj.GetType()) {
+                return false;
+            }
+
+            TabViewModel otherEntry = (TabViewModel)obj;
+            return this.Index == otherEntry.Index;
         }
     }
 }
