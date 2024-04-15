32352d5b5907ad5d1ec7650b4cccac2fcde6d95a
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-04-15 22:56:20 +0200

    #update methods

--- BetterEditor/MainWindow.xaml.cs
+++ BetterEditor/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using BetterEditor.ViewModels;
+using System;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
 using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace BetterEditor {
     /// <summary>
@@ -11,66 +13,50 @@ namespace BetterEditor {
     /// </summary>
     public partial class MainWindow : Window {
         MainViewModel _mainViewModel;
-        private string _overlap = "🗗";
-        private string _maximize = "🗖";
         private bool _canSetStateNormal = false;
 
         public MainWindow() {
             InitializeComponent();
             _mainViewModel = (MainViewModel)DataContext;
-            this.SizeChanged += MainWindowSizeChanged;
+            StateChanged += MainWindowStateChangeRaised;
         }
 
-        private void MainWindowSizeChanged(object sender, SizeChangedEventArgs e) {
-            _mainViewModel.ChangeSizeButton = (this.WindowState == WindowState.Maximized)? _overlap : _maximize;
+        // Can execute
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = true;
         }
 
-        private void MinimizeWindow(object sender, RoutedEventArgs e) {
-            WindowState = WindowState.Minimized;
+        // Minimize
+        private void CommandBinding_Executed_Minimize(object sender, ExecutedRoutedEventArgs e) {
+            SystemCommands.MinimizeWindow(this);
         }
 
-        private void ChangeWindowSize(object sender, RoutedEventArgs e) {
+        // Maximize
+        private void CommandBinding_Executed_Maximize(object sender, ExecutedRoutedEventArgs e) {
+            SystemCommands.MaximizeWindow(this);
+        }
+
+        // Restore
+        private void CommandBinding_Executed_Restore(object sender, ExecutedRoutedEventArgs e) {
+            SystemCommands.RestoreWindow(this);
+        }
+
+        // Close
+        private void CommandBinding_Executed_Close(object sender, ExecutedRoutedEventArgs e) {
+            SystemCommands.CloseWindow(this);
+        }
+
+        // State change
+        private void MainWindowStateChangeRaised(object sender, EventArgs e) {
             if (WindowState == WindowState.Maximized) {
-                WindowState = WindowState.Normal;
-            } else { 
-                WindowState = WindowState.Maximized;
+                MainWindowBorder.BorderThickness = new Thickness(8);
+                RestoreButton.Visibility = Visibility.Visible;
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            } else {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+                RestoreButton.Visibility = Visibility.Collapsed;
+                MaximizeButton.Visibility = Visibility.Visible;
             }
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-            if (e.ClickCount == 2) {
-                _canSetStateNormal = false;
-                if (WindowState == WindowState.Maximized) {
-                    WindowState = WindowState.Normal;
-                } else {
-                    WindowState = WindowState.Maximized;
-                }
-            }
-        }
-
-        private void MouseMoves(object sender, System.Windows.Input.MouseEventArgs e) {
-            if (e.LeftButton == MouseButtonState.Pressed) {
-                if (WindowState == WindowState.Maximized && _canSetStateNormal) {
-                    WindowState = WindowState.Normal;
-                    Screen selectedScreen = Screen.FromHandle(new System.Windows.Interop.WindowInteropHelper(this).Handle);
-                    int screenWidth = selectedScreen.WorkingArea.Width;
-                    double leftArea = screenWidth * 0.2;
-                    double middle = screenWidth * 0.8;
-                    int upperLeftCornor = selectedScreen.Bounds.Left;
-                    double mouseXPosition = e.GetPosition(this).X;
-                    Top = 0;
-                    if (mouseXPosition <= leftArea)
-                        Left = upperLeftCornor;
-                    else if (mouseXPosition > leftArea && mouseXPosition <= middle)
-                        Left = upperLeftCornor + mouseXPosition - Width / 2;
-                    else
-                        Left = upperLeftCornor + screenWidth - Width;
-                }
-                DragMove();
-            }
-        }
-        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
-            _canSetStateNormal = true;
         }
     }
 }
