240bfca94a912ede70047207438b7603fa13c34f
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-06-01 23:03:31 +0200

    #add commands and constructor

--- BetterEditor/ViewModels/ReplaceViewModel.cs
+++ BetterEditor/ViewModels/ReplaceViewModel.cs
@@ -3,8 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using VocabTrainer.Models;
 
 namespace BetterEditor.ViewModels {
     internal class ReplaceViewModel {
+        public ReplaceViewModel() { }
+        private bool CanExecuteCommand(object arg) {
+            return true;
+        }
+        public ICommand SearchViewCommand => new RelayCommand(SearchView, CanExecuteCommand);
+        private void SearchView(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand SearchCommand => new RelayCommand(Search, CanExecuteCommand);
+        private void Search(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand SearchNextCommand => new RelayCommand(SearchNext, CanExecuteCommand);
+        private void SearchNext(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand SearchPreviousCommand => new RelayCommand(SearchPrevious, CanExecuteCommand);
+        private void SearchPrevious(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand CloseReplaceBarCommand => new RelayCommand(CloseSearchBar, CanExecuteCommand);
+        private void CloseSearchBar(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand ReplaceCommand => new RelayCommand(Replace, CanExecuteCommand);
+        private void Replace(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
+
+        public ICommand ReplaceAllCommand => new RelayCommand(ReplaceAll, CanExecuteCommand);
+        private void ReplaceAll(object obj) {
+            try {
+                throw new NotImplementedException();
+            } catch (Exception e) {
+                BaseViewModel.ShowErrorMessage(e);
+            }
+        }
     }
 }
