8ac22e0cbbd004e2ecb3ca5cae67e5a2908b0750
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-07-09 23:25:12 +0200

    #remove field

--- BetterEditor/ViewModels/ReplaceViewModel.cs
+++ BetterEditor/ViewModels/ReplaceViewModel.cs
@@ -23,7 +23,6 @@ namespace BetterEditor.ViewModels {
         private ObservableCollection<TabViewModel> _tabs;
         private int _openedMatch = 0;
         private ObservableCollection<int> _matchingTabs = new ObservableCollection<int>();
-        private TabViewModel _tab;
         public ReplaceViewModel(TextEditorViewModel parent) { 
             _parent = parent;
             _tabs = _parent.UsedTabs;
