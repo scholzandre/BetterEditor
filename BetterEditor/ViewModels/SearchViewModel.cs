d09d97e125cbe9145a730cbb4fa2f204bdf829a9
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-07-09 23:25:04 +0200

    #update selecting text

--- BetterEditor/ViewModels/SearchViewModel.cs
+++ BetterEditor/ViewModels/SearchViewModel.cs
@@ -89,6 +89,7 @@ namespace BetterEditor.ViewModels {
                         MessageBox.Show(_noMatchesText);
                     if (_matchingTabs.Count > 0 && _openedMatch < _matchingTabs.Count) {
                         _parent.OpenTabCommand.Execute(_parent.UsedTabs[_matchingTabs[_openedMatch]]);
+                        _parent.RequestSelectText(_parent.UsedTabs[_matchingTabs[_openedMatch]].Content.IndexOf(SearchText), SearchText.Length);
                         _openedMatch++;
                     }
                 }
@@ -111,6 +112,7 @@ namespace BetterEditor.ViewModels {
                         MessageBox.Show(_noMatchesText);
                     if (_matchingTabs.Count > 0 && _openedMatch >= 0) {
                         _parent.OpenTabCommand.Execute(_parent.UsedTabs[_matchingTabs[_openedMatch]]);
+                        _parent.RequestSelectText(_parent.UsedTabs[_matchingTabs[_openedMatch]].Content.IndexOf(SearchText), SearchText.Length);
                         _openedMatch--;
                     }
                 }
