1ee494621ce53f9563097846cbce6c2a73f711db
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-08-15 22:32:56 +0200

    #add property and constructor

--- BetterEditor/ViewModels/ListTabsViewModel.cs
+++ BetterEditor/ViewModels/ListTabsViewModel.cs
@@ -5,6 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BetterEditor.ViewModels {
-    internal class ListTabsViewModel {
+    internal class ListTabsViewModel : BaseViewModel {
+        private string _searchText = string.Empty;
+        public string SearchText {
+            get => _searchText;
+            set {
+                _searchText = value.Trim();
+                OnPropertyChanged(nameof(SearchText));
+            }
+        }
+        public ListTabsViewModel() { }  
     }
 }
