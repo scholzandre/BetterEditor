d4f47b2dc71a94c60533218b036603d13d6de5d6
scholzandre <143190997+scholzandre@users.noreply.github.com>
2024-08-29 22:25:35 +0200

    #add RenameTabViewModel

---
+++ BetterEditor/ViewModels/RenameTabViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterEditor.ViewModels
+{
+    internal class RenameTabViewModel : BaseViewModel {
+        private string _filePath = string.Empty;
+        public string FilePath {
+            get => _filePath;
+            set { 
+                _filePath = value;
+                OnPropertyChanged(nameof(FilePath));
+            }
+        }
+        public RenameTabViewModel(string filePath) {
+            FilePath = filePath;
+        } 
+
+        ~RenameTabViewModel() { }
+    }
+}
